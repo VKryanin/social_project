4166f3974148b9537547d6ba67157cc6d7c85008
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2022-08-03 17:38:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        
+    <div className="app-wrapper">
+        <header className='header'>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/ZDF_logo%21_Logo_2021.svg/800px-ZDF_logo%21_Logo_2021.svg.png'/>
+        </header>
+        <nav className='nav'>
+          <div>
+            <a>Profile</a>
+          </div>
+          <div>
+            <a>Messages</a>
+          </div>
+          <div>
+            <a>News</a>
+          </div>
+          <div>
+            <a>Music</a>
+          </div>
+          <div>
+            <a>Profile</a>
+          </div>
+          <div>
+            <a>Setting</a>
+          </div>
+        </nav>
+        <div className='content'>Main content</div>
     </div>
   );
 }
