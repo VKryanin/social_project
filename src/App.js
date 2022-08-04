4a99932d2a7eb758f47ca9fc464de43b1014fe82
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2022-08-04 18:50:40 +0300

    18^50

--- src/App.js
+++ src/App.js
@@ -27,7 +27,28 @@ function App() {
             <a>Setting</a>
           </div>
         </nav>
-        <div className='content'>Main content</div>
+        <div className='content'>
+          <div>
+            <img src='https://images.ctfassets.net/hrltx12pl8hq/4MFiRr9vFnbWzYoNSPiYXy/fca130dd40da59b06e83ee8d5789a23e/file-converter-shutterstock.jpg'/>
+          </div>
+          <div>
+            ava
+          </div>
+          <div>
+            my posts
+            <div>
+              new post
+            </div>
+            <div>
+              <div>
+                post 1
+              </div>
+              <div>
+                post 2
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
