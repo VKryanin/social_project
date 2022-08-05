5d113425a051eb1e4af617ece273e04081e69b5a
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2022-08-05 17:45:16 +0300

    add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+
+export const Header= () => {
+    return  <header className='header'>
+    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/ZDF_logo%21_Logo_2021.svg/800px-ZDF_logo%21_Logo_2021.svg.png'/>
+    </header>
+}
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+export const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img src='https://images.ctfassets.net/hrltx12pl8hq/4MFiRr9vFnbWzYoNSPiYXy/fca130dd40da59b06e83ee8d5789a23e/file-converter-shutterstock.jpg'/>
+    </div>
+    <div>
+      ava
+    </div>
+    <div>
+      my posts
+      <div>
+        new post
+      </div>
+      <div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  </div>
+}
