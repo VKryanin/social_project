657e500d3609114796dda624e6e23282b9b7c99c
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2022-08-09 17:48:10 +0300

    add modules from classes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,7 @@
-import './Profile.css'
+import prof from './Profile.module.css'
 
 export const Profile = () => {
-    return <div className='content'>
+    return <div className={prof.content}>
     <div>
       <img src='https://images.ctfassets.net/hrltx12pl8hq/4MFiRr9vFnbWzYoNSPiYXy/fca130dd40da59b06e83ee8d5789a23e/file-converter-shutterstock.jpg'/>
     </div>
@@ -13,11 +13,11 @@ export const Profile = () => {
       <div>
         new post
       </div>
-      <div>
-        <div>
+      <div className={prof.posts}>
+        <div className={prof.item}>
           post 1
         </div>
-        <div>
+        <div className={prof.item}>
           post 2
         </div>
       </div>
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,24 +1,24 @@
-import './nav.css'
+import classes from './Navbar.module.css'
 
-export const Navbar = () => {
-    return <nav className='nav'>
-          <div className="item">
-            <a>Profile</a>
-          </div>
-          <div className="item">
-            <a>Messages</a>
-          </div>
-          <div className="item">
-            <a>News</a>
-          </div>
-          <div className="item">
-            <a>Music</a>
-          </div>
-          <div className="item">
-            <a>Profile</a>
-          </div>
-          <div className="item">
-            <a>Setting</a>
-          </div>
-        </nav>
+export function Navbar() {
+  return <nav className={classes.nav}>
+    <div className={classes.item}>
+      <a>Profile</a>
+    </div>
+    <div className={classes.item}>
+      <a>Messages</a>
+    </div>
+    <div className={classes.item}>
+      <a>News</a>
+    </div>
+    <div className={classes.item}>
+      <a>Music</a>
+    </div>
+    <div className={classes.item}>
+      <a>Profile</a>
+    </div>
+    <div className={classes.item}>
+      <a>Setting</a>
+    </div>
+  </nav>
 }
