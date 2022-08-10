2f5a7b7bf00fcaa6858f504ca4de867e961f4f25
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2022-08-10 19:01:18 +0300

    10.08

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import './Header.css';
+import s from './Header.module.css';
 
 export const Header= () => {
-    return  <header className='header'>
+    return  <header className={s.header}>
     <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/ZDF_logo%21_Logo_2021.svg/800px-ZDF_logo%21_Logo_2021.svg.png'/>
     </header>
 }
