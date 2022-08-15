e50705cc8f4e756cf1cccdde4624a814df3503c4
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2022-08-15 16:26:26 +0300

    15.08

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,8 +11,8 @@ export const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post message="Hi, how are you?" />
-                <Post message="It's my first post"/>
+                <Post message="Hi, how are you?"  like='1'/>
+                <Post message="It's my first post" like='15'/>
             </div>
         </div>)
 }
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -7,7 +7,7 @@ export const Post = (props) => {
                 <img src="https://i.pinimg.com/564x/67/89/71/67897168a4d6ef6f8d9c8b132562dac0.jpg" alt="" /> 
                 <span>{props.message}</span>
                 <div>
-                    <span>Like</span>
+                    <span>Like {props.like}</span>
                 </div>
             </div>)
 }
