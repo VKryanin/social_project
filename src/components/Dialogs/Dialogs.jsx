526be10d37f1665a71854dda2eee08a6fdc829ba
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2022-08-17 18:00:48 +0300

    add navlink

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,19 @@ import React from "react";
 
 export const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs} >
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog}>Sergey</div>
+                    <div className={s.dialog}>Alina</div>
+                    <div className={s.dialog}>Andrey</div>
+                    <div className={s.dialog}>Tolya</div>
+                    <div className={s.dialog}>Ksusha</div>
+                </div>
+            <div className={s.messages}>
+                <div className={s.message}>hi</div>
+                <div className={s.message}>How us your name</div>
+                <div className={s.message}>yo</div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/nav.jsx
+++ src/components/Navbar/nav.jsx
@@ -1,21 +1,22 @@
-import cl from './Navbar.module.css'
+import s from './Navbar.module.css'
+import { NavLink } from 'react-router-dom'
 
 export function Navbar() {
-  return <nav className={cl.nav}>
-    <div className={cl.item}>
-      <a href='/profile'>Profile</a>
+  return <nav className={s.nav}>
+    <div className={s.item}>
+      <NavLink to='/profile' className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
     </div>
-    <div className={`${cl.item} ${cl.active}`}>
-      <a href='/dialogs'>Messages</a>
+    <div className={s.item}>
+      <NavLink to='/dialogs' className = { navData => navData.isActive ? s.active : null }>Messages</NavLink>
     </div>
-    <div className={cl.item}>
-      <a href='/news' >News</a>
+    <div className={s.item}>
+      <NavLink to='/news' className = { navData => navData.isActive ? s.active : null }>News</NavLink>
     </div>
-    <div className={cl.item}>
-      <a href='/music'>Music</a>
+    <div className={s.item}>
+      <NavLink to='/music' className = { navData => navData.isActive ? s.active : null }>Music</NavLink>
     </div>
-    <div className={cl.item}>
-      <a href='/setting' >Setting</a>
+    <div className={s.item}>
+      <NavLink to='/setting' className = { navData => navData.isActive ? s.active : s.item }>Setting</NavLink>
     </div>
   </nav>
 }
