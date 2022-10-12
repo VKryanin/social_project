aabc7d61c122e663d1778cea47e36a2d5bf95038
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2022-10-12 17:32:01 +0300

    rework 2

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,8 +4,10 @@ import { Post } from './Post/Post'
 
 export const MyPosts = () => {
     let postsData = [
-        { id: 1, post: 'I\'ll become king of the pirates', likesCount: 12 },
-        { id: 2, post: 'Hello! I\`m Monkey D. Luffy', likesCount: 24 }
+        { id: 1, post: 'Hello! I\`m Monkey D. Luffy', likesCount: Math.floor(Math.random() * 1000), date: 'October 20, 1999' },
+        { id: 2, post: 'I\'ll become king of the pirates', likesCount: Math.floor(Math.random() * 2000), date: 'October 20, 1999' },
+        { id: 3, post: 'Today I met my new nakama. His name\`s Roronoa Zoro', likesCount: Math.floor(Math.random() * 3000), date: 'November 17, 1999' },
+        { id: 4, post: 'I defeated morgan', likesCount: Math.floor(Math.random() * 1000), date: 'November 24, 1999' }
     ]
 
     return (
@@ -20,8 +22,10 @@ export const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={postsData[0].post} like={postsData[0].likesCount} />
-                <Post message={postsData[1].post} like={postsData[1].likesCount} />
+                <Post message={postsData[3].post} like={postsData[3].likesCount} date={postsData[3].date} />
+                <Post message={postsData[2].post} like={postsData[2].likesCount} date={postsData[2].date} />
+                <Post message={postsData[1].post} like={postsData[1].likesCount} date={postsData[1].date} />
+                <Post message={postsData[0].post} like={postsData[0].likesCount} date={postsData[0].date} />
             </div>
         </div>)
 }
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -3,13 +3,20 @@ import React from 'react';
 
 export const Post = (props) => {
     return (
-            <div className={s.item}>
-                <img src="https://i.pinimg.com/564x/67/89/71/67897168a4d6ef6f8d9c8b132562dac0.jpg" alt="" /> 
-                <span>{props.message}</span>
-                <div>
-                    <span>Like {props.like}</span>
+        <div className={s.item}>
+            <div className={s.itemPost}>
+                <div className={s.avatar}>
+                    <img src="https://i.pinimg.com/564x/67/89/71/67897168a4d6ef6f8d9c8b132562dac0.jpg" alt="" />
                 </div>
-            </div>)
+                <div className={s.textPost}>
+                    <span>{props.message}</span>
+                </div>
+            </div>
+            <div className={s.CountLikes}>
+                <span className={s.date}>{props.date}</span>
+                <span>Like {props.like}</span>
+            </div>
+        </div>)
 }
 
 // export default Post
